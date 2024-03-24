6817329395b604014991be9ffe9ab46201c03075
patrlean <98681203+patrlean@users.noreply.github.com>
2024-03-24 23:02:39 +0800

    Update train_model.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -7,8 +7,8 @@
 # LICENSE file in the root directory of this source tree.
 
 import sys
-sys.path.insert(0, os.path.abspath('.'))
 import os
+sys.path.insert(0, os.path.abspath('.'))
 
 import argparse
 import json
