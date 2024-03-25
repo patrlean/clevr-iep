eaa81d2100e6090c49c5e4cebd06040637229c26
patrlean <98681203+patrlean@users.noreply.github.com>
2024-03-25 11:15:59 +0800

    Update seq2seq.py

--- iep/models/seq2seq.py
+++ iep/models/seq2seq.py
@@ -170,7 +170,7 @@ class Seq2Seq(nn.Module):
             # logprobs is N x 1 x V
             logprobs, h, c = self.decoder(encoded, cur_input, h0=h, c0=c)
             logprobs = logprobs / temperature
-            probs = F.softmax(logprobs.view(N, -1)) # Now N x V
+            probs = F.softmax(logprobs.view(N, -1), dim=1) # Now N x V
             if argmax:
                 _, cur_output = probs.max(1)
             else:
